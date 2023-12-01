90eca0cb7c8d21ef9a1bcf4e0a2ad58f5ab11d7e
Yashwanth <yashwanthpothala04@gmail.com>
2023-12-01 18:39:52 +0530

    Added home

---
+++ HOME/home.js
@@ -0,0 +1,7 @@
+function submitQuestion() {
+    // Add JavaScript logic for handling the submission of questions
+    alert("Question submitted! Our team will get back to you shortly.");
+  }
+  
+  // Add any additional JavaScript functionality here
+  
